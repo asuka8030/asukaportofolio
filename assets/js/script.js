d490e9d8539864086ae6214860b9e91f4981edc5
asuka8030 <asuka8030@hotmail.co.jp>
2023-08-02 11:13:22 +0900

    works完成

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(function () {
     $(".tabs-item").removeClass("active");
     $(this).addClass("active");
 
-    let index = $(this).index();
+    let index = $(".tabs-item").index(this);
 
     $(".content").removeClass("active");
     $(".content").eq(index).addClass("active");
